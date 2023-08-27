33c462041b00d7df06ae9ecee6fa3cccfaac330e
Donald Nguyen <donaldnguyen99@users.noreply.github.com>
2023-08-27 00:59:44 -0700

    Add error handling

--- flashscore-open-transfers.py
+++ flashscore-open-transfers.py
@@ -262,4 +262,8 @@ def main():
     open_pages(urls)
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(e)
+        input('Press Enter to exit... ')
